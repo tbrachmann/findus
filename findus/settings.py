e50ca1fa7b0d9146e712c10bd5d20a33b036cb34
Tobias Rainer Brachmann <tobybrachmann@gmail.com>
2025-09-09 09:28:13 -0700

    secret key from env

--- findus/settings.py
+++ findus/settings.py
@@ -28,6 +28,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
+SECRET_KEY = os.getenv('SECRET_KEY', 'SECRET_KEY')
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
